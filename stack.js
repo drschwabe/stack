d2bccd62896772ef80f5daec3a0c6174ff226c3c
MattMcFarland <contact@mattmcfarland.com>
2017-06-08 21:11:18 -0400

    WIP stuff

--- stack.js
+++ stack.js
@@ -2,61 +2,36 @@ var async = require('async'),
     _ = require('underscore'), 
     routeParser = require('route-parser')
 
-var stack = { 
-  routes : [], 
-  state : {}, 
-  command_queue : [], 
-  next_queue : []
+function Stack () {
+  this.routes = []
+  this.state = {}
+  this.command_queue = []
+  this.next_queue = []  
+  this.commands = {}  
+  this._routes = []
 }
 
-stack.on = function(param1, callback) {
-  //param1: a string or an array of strings.
-  //(is either a single path or array of paths)  
+const normalizeArguments = (...args) => Array.isArray(args[0]) ? [].concat(args[0], ...args.slice(1)) : args
 
-  var that = this
-  var registerRoute = function(path, listenerCallback) {
-
-    var route = new routeParser(path)
-    var existingRoute = _.find(that.routes, function(existingRoute) {
-      return existingRoute.route.match(path)      
-    })
-    //The newMiddleware contains two properties; one is the callback
-    //the other is the full path so we can later target/override this. 
-    var newMiddleware = { func : listenerCallback, path: path }    
-    // wildcard paths naturally do not get added to other routes,
-    // instead other paths are added to wildcard routes only after they
-    // are defined.  Because we want wildcard paths to also work with prior
-    // defined routes, then we must add the wildcard paths to the middlewares
-    // of the other routes.
-    // This could get tricky though, and more testing is needed to make sure
-    // this does not introduce even more problems.
-    var isWild = (~path.indexOf('*'))
-    if (isWild) {
-      that.routes = that.routes.map(routes => Object.assign({}, routes, {middleware: [...routes.middleware, newMiddleware]}))
-    }
-    //Determine if the route already exists:
-    if(!existingRoute) {
-      route = { route: route, middleware: [newMiddleware] }
-      //Make an entry for it; add to known routes and define middleware array/stack:
-      that.routes.push(route)
-    } else {
-      //If the route already exists, just push the new middleware into the 
-      //existing stack: 
-      existingRoute.middleware.push(newMiddleware)
-    }
-  }
-
-  //If an array was not supplied, create an array anyway: 
-  var paths
-  if(!_.isArray(param1)) paths = [param1]
-  else paths = param1
+Stack.prototype.on = function() {  
+  const args = normalizeArguments(Array.from(arguments))
+  const paths = args.slice(0, -1);
+  const callback = args[args.length - 1];
+  const registerRoute = assignRouteRegister(this);
+  this.commands = paths.reduce((acc, path) => {
+    const fns = this.commands[path] && this.commands[path].fns || [];
+    const route = this.commands[path].route
+    const commandsForPath = this.commands[path] ? this.commands[path] : { fns: []};
+    acc[path] = [].concat(commandsForPath, [path, callback]);
+    return acc;
+  }, {})
 
   paths.forEach(function(path) {
     registerRoute(path, callback)
   })
 }
 
-stack.fire = function(path, param2, param3) {
+Stack.prototype.fire = function(path, param2, param3) {
 
   var state, callback
   //Parse the parameters to figure out what we got: 
@@ -82,21 +57,16 @@ stack.fire = function(path, param2, param3) {
   //At this point if there is already a stack._command it means there is
   //a parent fire already in progress. 
   //So we store it so it can be applied after this particular fire completes. 
-  if(state._command) stack.command_queue.push(state._command)
-
-  var matchingRoute, command
-  matchingRoute = _.find(this.routes, function(route) {
-    var result = route.route.match(path)
-    if(result) {
-      command = result
-    } else if(!result || _.isUndefined(result)) {
-      command = {} //< If there was no matching route, create an obj anyway.       
-    }
+  if(state._command) this.command_queue.push(state._command)
+  var command
+  var matchingRoute = _.find(this.routes, function(route) {
+    var result = route.route.match(path)    
+    command = undefined !== result && result || {}
     command.path = path
     //^ Parses the route; organizing params into a tidy object.
     return result
   })
-
+  
   var that = this
 
   //Apply command as a property of state. 
@@ -104,6 +74,7 @@ stack.fire = function(path, param2, param3) {
 
   async.waterfall([
     function(seriesCallback) {
+      
       var seedFunction = function(next) { next(null, state) }
       if(matchingRoute) {      
         //Give the waterfall a seed function with null error, parsed/matched route (req), and state: 
@@ -139,7 +110,53 @@ stack.fire = function(path, param2, param3) {
       if(_.isFunction(callback)) callback(null, state)                
     }
   ])
+
+  
 }
 
+function assignRouteRegister (stack) {
+  return function(path, listenerCallback) {
 
-module.exports = stack
+    var route = new routeParser(path)
+    var existingRoute = _.find(stack.routes, function(existingRoute) {    
+      return existingRoute.route.match(path)      
+    })
+    
+    //The newMiddleware contains two properties; one is the callback
+    //the other is the full path so we can later target/override this. 
+    var newMiddleware = { func : listenerCallback, path: path }    
+    // wildcard paths naturally do not get added to other routes,
+    // instead other paths are added to wildcard routes only after they
+    // are defined.  Because we want wildcard paths to also work with prior
+    // defined routes, then we must add the wildcard paths to the middlewares
+    // of the other routes.
+    // This could get tricky though, and more testing is needed to make sure
+    // this does not introduce even more problems.
+    var isWild = (~path.indexOf('*'))
+    if (isWild) {
+      stack.routes = stack.routes.map(routes => Object.assign({}, routes, {middleware: [...routes.middleware, newMiddleware], wild: true}))      
+    }
+    //Determine if the route already exists:
+    console.log('checking route:', route.spec)
+    if(!existingRoute) {
+      console.log('create new route:', route.spec)
+      console.log('add', path, 'to new route:', route.spec)
+      route = { route: route, middleware: [newMiddleware] }
+      //Make an entry for it; add to known routes and define middleware array/stack:      
+      stack.routes.push(route)
+    } else if (isWild) {
+      console.log('add WILD', path, 'to existing route:', existingRoute.route.spec)
+      //If the route already exists, just push the new middleware into the 
+      //existing stack: 
+      existingRoute.middleware.push(newMiddleware)
+    } else if (~existingRoute.route.spec.indexOf('*')) {
+      console.log('add', path, 'to existing WILD route:', existingRoute.route.spec)
+      existingRoute.middleware.push(newMiddleware)
+    } else {
+      console.log('add', path, 'to existing route:', existingRoute.route.spec)
+      existingRoute.middleware.push(newMiddleware)
+    }
+  }
+}
+
+module.exports = new Stack()
